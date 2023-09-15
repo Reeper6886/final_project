faa70d402d161ed54384e30b00f627b1c63908d4
Rebecca Lee <sylvester_pluto@hotmail.com>
2023-09-15 04:50:52 -0400

    Emotion Detector

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -5,7 +5,7 @@ import requests
 
 def emotion_detector(text_to_analyze):
     """
-    Emotion Detection funtion
+    Emotion Detection function
     """
     url = (
         'https://sn-watson-emotion.labs.skills.network/'
@@ -16,15 +16,25 @@ def emotion_detector(text_to_analyze):
     response = requests.post(url, json = myobj, headers=header)
     response_json = json.loads(response.text)
 
-    emotion_scores = response_json['emotionPredictions'][0]['emotion']
-    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
-    desired_emotion = {
-        'anger': emotion_scores['anger'],
-        'disgust': emotion_scores['disgust'],
-        'fear': emotion_scores['fear'],
-        'joy': emotion_scores['joy'],
-        'sadness': emotion_scores['sadness'],
-        'dominant_emotion': dominant_emotion
-    }
+    if response.status_code == 200:
+        emotion_scores = response_json['emotionPredictions'][0]['emotion']
+        dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+        desired_emotion = {
+            'anger': emotion_scores['anger'],
+            'disgust': emotion_scores['disgust'],
+            'fear': emotion_scores['fear'],
+            'joy': emotion_scores['joy'],
+            'sadness': emotion_scores['sadness'],
+            'dominant_emotion': dominant_emotion
+        }
+    elif response.status_code == 400:
+        desired_emotion = {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
 
     return desired_emotion
--- server.py
+++ server.py
@@ -10,15 +10,22 @@ app=Flask("Emotion Detector")
 @app.route('/emotionDetector')
 def emotion_detection():
     ''' This code receives the text from the HTML interface and 
-        runs sentiment analysis over it using sentiment_analysis()
+        runs emotion detection over it using emotion_detection()
         function. The output returned shows the label and its confidence 
         score for the provided text.
     '''
     text_to_analyze = request.args.get('textToAnalyze')
     response=emotion_detector(text_to_analyze)
+
     if response['dominant_emotion'] is None:
-        return "invalid input! "
-    return ("For the given statement, the system response is anger: {}, disgust: {}, fear: {}, joy: {} and sadness:{}, The dominant emotion is {} ".format(response['anger'],response['disgust'],response['fear'],response['joy'],response['sadness'],response['dominant_emotion'] ),200 )
+        return "Invalid text! Please try again!."
+    #Displaying in f-string for return statement for screenshot
+    return (
+        f"For the given statement, the system response is:"
+        f"Anger: {response['anger']}, Disgust: {response['disgust']},"
+        f"Fear: {response['fear']}, Joy: {response['joy']},"
+        f"Sadness: {response['sadness']}, Dominant Emotion: {response['dominant_emotion']}"
+    ), 200
 
 @app.route('/')
 def index():
